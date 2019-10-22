45035a267a78febd09e3d40ce1ccb190cd0d3e81
Hernán Valdés <hfvaldesg@gmail.com>
2019-10-22 14:35:03 +0200

    Separate the flow of decision for the different args

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ subparser = parser.add_subparsers()
 
 # Subparsers task
 subparser_task = subparser.add_parser("task", help="Add a new task")
-subparser_task.add_argument("-n", "--new", action="store", dest="new_task", 
-                    help="description of task")
+subparser_task.add_argument("-n", "-new", dest="new_task", 
+                    help="description of task", action="store", default=argparse.SUPPRESS)
 
 # Subparser list all task
 subparser_task_list = subparser.add_parser("list", help="Show all the task running")
-subparser_task_list.add_argument("-s", "--show")
+subparser_task_list.add_argument("-s", "-show", action="store_true", dest="show")
 
 args = parser.parse_args()
-print(args.new_task)
+if "new_task" in args:
+    print(args.new_task)
+
+if "show" in args:
+    print("Printing list ...!") 
