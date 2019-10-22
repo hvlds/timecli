6dd377aa42d2559bc93fbbac0e670a6467fd5a6b
Hernán Valdés <hfvaldesg@gmail.com>
2019-10-22 13:07:01 +0200

    Add subparses to separete group of task

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Manage your task")
-parser.add_argument("-t", "--task", dest="task", required=True,
+subparser = parser.add_subparsers()
+
+# Subparsers task
+subparser_task = subparser.add_parser("task", help="Add a new task")
+subparser_task.add_argument("-n", "--new", action="store", dest="new_task", 
                     help="description of task")
 
+# Subparser list all task
+subparser_task_list = subparser.add_parser("list", help="Show all the task running")
+subparser_task_list.add_argument("-s", "--show")
+
 args = parser.parse_args()
-print(args.task)
+print(args.new_task)
