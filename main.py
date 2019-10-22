fee4f1eb4f8719e15087596300193ed75e77c8d0
Hernán Valdés <hfvaldesg@gmail.com>
2019-10-22 15:09:20 +0200

    Reformat code and add new argument in tasks subparser

--- main.py
+++ main.py
@@ -4,13 +4,31 @@ parser = argparse.ArgumentParser(description="Manage your task")
 subparser = parser.add_subparsers()
 
 # Subparsers task
-subparser_task = subparser.add_parser("task", help="Add a new task")
-subparser_task.add_argument("-n", "-new", dest="new_task", 
-                    help="description of task", action="store", default=argparse.SUPPRESS)
+subparser_task = subparser.add_parser(
+    "task", 
+    help="Add a new task")
+
+# new task argument
+subparser_task.add_argument(
+    "-n", "-new", 
+    dest="new_task", 
+    help="new task", 
+    action="store", 
+    default=argparse.SUPPRESS)
+
+# kill task argument
+subparser_task.add_argument(
+    "-k", "-kill", 
+    help="kill a task",
+    dest="kill_task",
+    action="store")
 
 # Subparser list all task
 subparser_task_list = subparser.add_parser("list", help="Show all the task running")
-subparser_task_list.add_argument("-s", "-show", action="store_true", dest="show")
+subparser_task_list.add_argument(
+    "-s", "-show", 
+    action="store_true", 
+    dest="show")
 
 args = parser.parse_args()
 if "new_task" in args:
