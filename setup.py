7b347f282a5d3b39a400faec4dd8e5ce2744b838
Hernán Valdés <hfvaldesg@gmail.com>
2019-10-29 15:37:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     version="0.1dev",
     name="timecli",
     author="Hernán Valdés",
+    description="Time Tracking CLI App",
+    author_email="hfvaldesg@gmail.com",
+    packages=["timecli"],
+    install_requires=["sqlalchemy"],
 )
